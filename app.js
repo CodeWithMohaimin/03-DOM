8bd9c8b2cb7ef27818e361525276467d7438a207
programmermohaimin <you@example.com>
2022-05-23 16:43:28 +0600

    now copy time

---
+++ app.js
@@ -0,0 +1,60 @@
+// window.onload = () =>{
+//     main()
+// }
+
+// function main() {
+//     const root = document.getElementById('root');
+//     const btn = document.getElementById('btn');
+//     const output = document.getElementById('output');
+
+//     btn.addEventListener('click', function () {
+//         const bgColor = HEX()
+//         root.style.backgroundColor = bgColor;
+//         output.value = bgColor
+//     })
+    
+// }
+
+// function HEX() {
+//     // HEX = #000000  --> #ffffff
+    
+//     const red = Math.floor(Math.random() * 255);
+//     const green = Math.floor(Math.random() * 255);
+//     const blue = Math.floor(Math.random() * 255);
+
+//     return `#${red.toString(16)}${blue.toString(16)}${green.toString(16)}`
+// }
+
+
+// now try unseen :)
+
+
+
+window.onload = () => {
+    main()
+}
+function main() {
+    // collect all references
+    const root = document.getElementById('root');
+    const btn = document.getElementById('btn');
+    const output = document.getElementById('output');
+
+    btn.addEventListener('click', function () {
+        const bgColor = hex()
+        root.style.backgroundColor = bgColor;
+        output.value = bgColor
+    })
+    
+}
+
+
+//generate Color code function
+function hex() {
+    
+    const red = Math.floor(Math.random() * 255);
+    const green = Math.floor(Math.random() * 255);
+    const blue = Math.floor(Math.random() * 255);
+
+    return `#${red.toString(16)}${green.toString(16)}${blue.toString(16)}`
+
+}
